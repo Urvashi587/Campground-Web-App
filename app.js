df87c0ce07b3bd83d95b14527f57055f4ee71b28
Urvashi Sinha <urvashisinha.2003@gmail.com>
2025-03-27 05:19:53 +0530

    modified db_url

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ const ExpressError=require('./utils/ExpressError')
 const mongoSanitize = require('express-mongo-sanitize');
 const MongoStore = require('connect-mongo');
 // const db_url=process.env.DB_URL;
-const db_url='mongodb://localhost:27017/yelp-camp';
+const db_url=process.env.DB_URL;
 mongoose.connect(db_url)
 const db=mongoose.connection;
 db.on("error",console.error.bind(console,"connection error"));
